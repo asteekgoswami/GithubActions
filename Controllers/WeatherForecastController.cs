ca3801ff71e874bce64e36831d5010b09d7d27db
Asteek Goswami <asteek.goswami@haymarket.com>
2025-03-19 17:49:11 +0530

    changes in controller

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -6,6 +6,7 @@ namespace GitHubActions_WebAPI.Controllers;
 [Route("[controller]")]
 public class WeatherForecastController : ControllerBase
 {
+    //add comment
     private static readonly string[] Summaries = new[]
     {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
